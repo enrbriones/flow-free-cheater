faca5ceb41f902adcaddc72a8f1773461ebba8db
Enrique Briones <enr.briones@outlook.cl>
2020-03-27 06:01:27 -0300

    always get better

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Flow-Free Cheater
+
+# [Online Demo](https://github.com/enrbriones/numberlink-solver-apirest)
+
+---
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -4,7 +4,7 @@ import {FaGithub } from 'react-icons/fa';
 const Footer = () => {
     return (
         <div className='mt-3'>
-          <p>Created by <a className='h5 text-primary' href='https://github.com/enrbriones' target='blank'>enrbriones</a> - <a className='h5' href='https://github.com/enrbriones' target='blank'><span><FaGithub size='30px'/></span> View GitHub project</a></p>  
+          <p>Created by <a className='h5 text-primary' href='https://github.com/enrbriones' target='blank'>enrbriones</a> - <a className='h5' href='https://github.com/enrbriones/numberlink-solver-apirest' target='blank'><span><FaGithub size='30px'/></span> View GitHub project</a></p>  
         </div>
     );
 };
